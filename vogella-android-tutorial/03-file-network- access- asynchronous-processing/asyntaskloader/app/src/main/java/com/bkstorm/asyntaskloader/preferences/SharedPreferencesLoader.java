6cce3953165adee3b1e2f49434ce7932c514420f
bkstorm <hoangnv@outlook.com>
2015-09-27 15:34:13 +0700

    - nothing

---
+++ vogella-android-tutorial/03-file-network- access- asynchronous-processing/asyntaskloader/app/src/main/java/com/bkstorm/asyntaskloader/preferences/SharedPreferencesLoader.java
@@ -0,0 +1,7 @@
+package com.bkstorm.asyntaskloader.preferences;
+
+/**
+ * Created by hoangnv on 22/08/2015.
+ */
+public class SharedPreferencesLoader {
+}
---
+++ vogella-android-tutorial/03-file-network- access- asynchronous-processing/sqlite-first/app/src/main/java/com/bkstorm/sqlitefirst/CommentsDataSource.java
@@ -0,0 +1,7 @@
+package com.bkstorm.sqlitefirst;
+
+/**
+ * Created by hoangnv on 18/08/2015.
+ */
+public class CommentsDataSource {
+}
